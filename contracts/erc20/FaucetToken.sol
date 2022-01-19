bde4fe66b3b6356c7b5d26a7b4377c494a44929a
Fan Wang <fan.wang@confluxnetwork.org>
2022-01-19 18:49:40 +0800

    feat: add FaucetToken for test

---
+++ contracts/erc20/FaucetToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+import "./ERC20.sol";
+
+contract FaucetToken is ERC20 {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) ERC20(_name, _symbol, _decimals) {}
+
+    function mint(address _to, uint256 _amount) external {
+        _mint(_to, _amount);
+    }
+
+    function burn(uint256 _amount) external {
+        _burn(msg.sender, _amount);
+    }
+}
